32aba1caf8d30a65860e1cf0ce1693fbc57d426e
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2022-02-23 00:16:04 -0500

    cleaned up variables & added bg color to postActionStyles()

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // grab elements
 const form = document.getElementById("sign-up-form");
 const firstName = document.getElementById("name");
-const allInputs = document.getElementsByClassName("label");
-console.log(allInputs);
 
 // post form actions
 form.addEventListener("submit", (e) => {
@@ -13,6 +11,8 @@ form.addEventListener("submit", (e) => {
 // inline styling for post submit
 function postActionStyles() {
   document.body.style.textAlign = "center";
+  document.body.style.fontFamily = "'Hammersmith One', sans-serif";
+  document.body.style.backgroundColor = "#F2F2F2";
   document.body.style.fontSize = "40px";
-  document.body.textContent = `Thanks for signing up, ${firstName.value}!`;
+  document.body.textContent = `Thanks for signing up, ${firstName.value.toUpperCase()}!`;
 }
